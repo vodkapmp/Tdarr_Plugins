5d79de2e7f36134c740f5d45f12e3de0ddee75cd
vodkapmp <70449815+vodkapmp@users.noreply.github.com>
2020-09-02 07:44:43 +0200

    Update Tdarr_Plugin_vdka_Chromecast_Compatibility.js

--- Community/Tdarr_Plugin_vdka_Chromecast_Compatibility.js
+++ Community/Tdarr_Plugin_vdka_Chromecast_Compatibility.js
@@ -10,7 +10,8 @@ function details() {
                      The plugin will make sure video is h264, has no subs, has no image streams, has compatible audio, is an mp4 file, and removes any unnecesary metadata.
                      CRF value for quality when transocding to h264 is configurable, defaults to 23 if not set.
                      Maximum bitrate is configurable, for situations where the server has constrained upload, this should NOT be used otherwise.
-                     FFmpeg preset is configurable, uses slow by default.\n\n`,
+                     FFmpeg preset is configurable, uses slow by default.
+                     FFmpeg currently does not support 7.1 EAC3 encoding, any non-EAC3 7.1 tracks will be downmixed to 5.1 AC3\n\n`,
         Version: "1.00",
         Link: "https://github.com/HaveAGitGat/Tdarr_Plugins/blob/master/Community/Tdarr_Plugin_vdka_Chromecast_Compatibility.js",
         Tags: "pre-processing,ffmpeg,h264,configurable",
@@ -124,15 +125,16 @@ module.exports.plugin = function plugin(file, librarySettings, inputs) {
 
     // Go through each stream for video streams in the file.
     for (var i = 0; i < file.ffProbeData.streams.length; i++) {
+        var streamX = file.ffProbeData.streams[i];
         // Check if stream is a video.
-        if (file.ffProbeData.streams[i].codec_type.toLowerCase() == "video") {
+        if (streamX.codec_type.toLowerCase() == "video") {
             // Check if codec  of stream is mjpeg/png, if so then remove this "video" stream. mjpeg/png are usually embedded pictures that can cause havoc with plugins.
-            if (file.ffProbeData.streams[i].codec_name == "mjpeg" || file.ffProbeData.streams[i].codec_name == "png") {
+            if (streamX.codec_name == "mjpeg" || streamX.codec_name == "png") {
                 removeimages += `-map -v:${videoIdx} `;
             }
 
             // Check if codec of stream is h264 AND check if it is mp4. If so just copy this video part
-            if (file.ffProbeData.streams[i].codec_name == "h264" && file.container == "mp4") {
+            if (streamX.codec_name == "h264" && file.container == "mp4") {
                 response.infoLog += `☑Video already h264 and .mp4. Video stream will not be transcoded. \n`;
                 response.preset = `,-map 0 -c:v:${videoIdx} copy `;
                 response.processFile = true;
@@ -140,18 +142,18 @@ module.exports.plugin = function plugin(file, librarySettings, inputs) {
             }
 
             // Check if codec of stream is 264 AND check if it is mp4. If so remux file.
-            if (file.ffProbeData.streams[i].codec_name == "h264" && file.container != "mp4") {
+            if (streamX.codec_name == "h264" && file.container != "mp4") {
                 response.infoLog += `☒Video is h264 but is not .mp4 container. Video stream will not be transcoded but file will be remuxed. \n`;
                 response.preset = `,-map 0 -c:v:${videoIdx} copy `;
                 response.processFile = true;
             }
 
             // Check if codec of stream is 264, if not, transcode!
-            if (file.ffProbeData.streams[i].codec_name != "h264") {
+            if (streamX.codec_name != "h264") {
                 response.infoLog += `☒Video is not h264, will be transcoded. Max bitrate set to ${inputs.max_bitrate}\n`;
                 response.preset = `,-map 0 -c:v:${videoIdx} libx264 -preset ${ffmpeg_preset} -crf ${crf} ${max_bitrate} `;
                 response.processFile = true;
-                // Increment videoIdx.
+            // Increment videoIdx.
                 videoIdx++;
             }
         }
@@ -164,49 +166,70 @@ module.exports.plugin = function plugin(file, librarySettings, inputs) {
 
         // Go through each stream in the file.
         for (var i = 0; i < file.ffProbeData.streams.length; i++) {
+            var streamX = file.ffProbeData.streams[i];
             // Check if stream is audio.
-            if (file.ffProbeData.streams[i].codec_type.toLowerCase() == "audio") {
+            if (streamX.codec_type.toLowerCase() == "audio") {
                 // Check if stream is 8 channel audio that isn't EAC3, convert to EAC3 if not
-                if (file.ffProbeData.streams[i].channels == "8" && file.ffProbeData.streams[i].codec_name != "eac3") {
-                    audioandsubcommandinsert += `-c:a:${audioIdx} eac3 `;
-                    response.infoLog += `☒Audio track ${audioIdx} is 8 channel, but not EAC3, converting to EAC3. \n`;
+                if (streamX.channels == "8" && streamX.codec_name != "eac3") {
+                    audioandsubcommandinsert += `-c:a:${audioIdx} ac3 `;
+                    response.infoLog += `☒Audio track ${audioIdx} is 8 channel, but not EAC3, downmixing to AC3 6ch. \n`;
                     audioIdx++;
                     continue;
                 }
 
                 // Check if stream is 6 channel audio that isn't AC3, convert to AC3 if not
-                if (file.ffProbeData.streams[i].channels == "6" && file.ffProbeData.streams[i].codec_name != "ac3") {
+                if (streamX.channels == "6" && streamX.codec_name != "ac3") {
                     audioandsubcommandinsert += `-c:a:${audioIdx} ac3 `;
                     response.infoLog += `☒Audio track ${audioIdx} is 6 channel, but not AC3, converting to AC3. \n`;
                     audioIdx++;
                     continue;
                 }
 
-                // Check if stream is 2 channel audio that isn't AAC, mp3, or opus, convert to AAC if not
-                if (file.ffProbeData.streams[i].channels == "2" && !["aac", "mp3", "opus"].includes(file.ffProbeData.streams[i].codec_name)) {
+                // Check if stream is 2 channel audio that isn't AAC, MP3, or Opus, convert to AAC if not
+                if (streamX.channels == "2" && !["aac", "mp3", "opus"].includes(streamX.codec_name)) {
                     audioandsubcommandinsert += `-c:a:${audioIdx} aac `;
-                    response.infoLog += `☒Audio track ${audioIdx} is 2 channel, but not AAC, mp3, or opus, converting to AAC. \n`;
+                    response.infoLog += `☒Audio track ${audioIdx} is 2 channel, but not AAC, MP3, or Opus, converting to AAC. \n`;
                     audioIdx++;
                     continue;
                 }
 
-                // Check if stream is 1 channel audio that isn't AAC, mp3, or opus, convert to AAC if not
-                if (file.ffProbeData.streams[i].channels == "1" && !["aac", "mp3", "opus"].includes(file.ffProbeData.streams[i].codec_name)) {
+                // Check if stream is 1 channel audio that isn't AAC, MP3, or Opus, convert to AAC if not
+                if (streamX.channels == "1" && !["aac", "mp3", "opus"].includes(streamX.codec_name)) {
                     audioandsubcommandinsert += `-c:a:${audioIdx} aac `;
-                    response.infoLog += `☒Audio track ${audioIdx} is 1 channel, but not AAC, mp3, or opus, converting to AAC. \n`;
+                    response.infoLog += `☒Audio track ${audioIdx} is 1 channel, but not AAC, MP3, or Opus, converting to AAC. \n`;
                     audioIdx++;
                     continue;
 
+                // If stream is compatible, set to copy stream
                 } else {
-                    response.infoLog += `☑Audio stream ${audioIdx} is compatible, will be copied. \n`
+                    response.infoLog += `☑Audio track ${audioIdx} is compatible. Will not be transcoded \n`
                     audioandsubcommandinsert += `-c:a:${audioIdx} copy `
-                    audiocopy = true;
                 }
 
                 // Increment audioIdx
                 audioIdx++;
             }
         }
+
+        // Check if audio needs to be transcoded or not, if all streams are compatible, audiocopy is set to true
+        // Go through each stream 
+        for (var i = 0; i < file.ffProbeData.streams.length; i++) {
+            var streamX = file.ffProbeData.streams[i];
+
+            // Check if stream is audio, if not slap a continue; on it
+            if (streamX.codec_type.toLowerCase() != "audio")
+                      continue;
+
+            // Check if stream is in compatible codec, if it isn't, then set audiocopy to false, and break the loop
+            if (!["aac", "mp3", "opus", "ac3", "eac3"].includes(streamX.codec_name)) {
+                      audiocopy = false;
+                      break;
+                }
+
+            // Check if stream is a compatible audio format, set audiocopy = true if it is
+            if (["aac", "mp3", "opus", "ac3", "eac3"].includes(streamX.codec_name))
+                      audiocopy = true;
+                }
 
 
 //
@@ -242,10 +265,10 @@ module.exports.plugin = function plugin(file, librarySettings, inputs) {
 //
 
 
-        // Check if all codecs checked out as compatible
+        // Check if all codecs checked out as compatible, if they do, return processFile = false
         response.processFile = !(videocopy && audiocopy && nosubs);
         if (!response.processFile) {
-            response.infoLog += "☑File should be fully chromecast compatible. File not being processed. \n";
+            response.infoLog += "☑File should be fully chromecast compatible. File will not be processed. \n";
             return response;
         }
     }
